4f5db96e9afb0cc73ba4fc1e90c5c43c9978e067
Sergey <smagometov649@gmail.com>
2024-02-07 18:36:44 +0200

    fix mobile adapt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
+'use client'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import { ThemeProvider } from "@emotion/react";
-import theme from "./theme"
+// import { ThemeProvider } from "@emotion/react";
+// import theme from "./theme"
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Forecaste App",
-  description: "MVP",
-};
+// export const metadata: Metadata = {
+//   title: "Forecaste App",
+//   description: "MVP",
+// };
 
 export default function RootLayout({
   children,
@@ -21,7 +22,7 @@ export default function RootLayout({
     <html lang='en'>
       {/* <body className={inter.className}><ThemeProvider theme={}>{children}</ThemeProvider></body> */}
       {/* <ThemeProvider theme={theme}> */}
-      <body className={inter.className}>
+      <body className={inter.className}  suppressHydrationWarning={true}>
         <Header />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,55 @@
 import styles from "./page.module.scss";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
-import phones from "../../public/Mockups.png"
+import phones from "../../public/Mockups.png";
+import gallary from "../../public/gallary.png"
 export default function Home() {
   return (
     <Box>
       <Grid container className={styles.main}>
-        <Grid item className={styles.content1} xl={12} lg={12} md={12} sm={12} xs={12}>
-          <Grid item xl={5} md={5} className={styles.hero}>
-            <p className={styles.text1}>Your Live </p>
-            <p className={styles.text2}>Forecaster</p>
-            <p className={styles.text3}>
+        <Grid item container className={styles.content1} xs={12}>
+          <Grid item xl={5} md={5} xs={11} className={styles.hero}>
+            <Typography className={`${styles.text} ${styles.text1}`}>
+              Your Live{" "}
+            </Typography>
+            <Typography className={`${styles.text} ${styles.text2}`}>
+              Forecaster
+            </Typography>
+            <Typography className={`${styles.text} ${styles.text3}`}>
               Together We Forecast: Building Dreams,
-            </p>
-            <p className={styles.text4}>One Day at a Time!</p>
-            <p className={styles.text5}>
+            </Typography>
+            <Typography className={`${styles.text} ${styles.text4}`}>
+              One Day at a Time!
+            </Typography>
+            <Typography className={`${styles.text} ${styles.text5}`}>
               1million People like you have purchased this product!
-            </p>
-            <Grid item xs={12} className={styles.btnGroup}>
-              <Grid item xs={2}>
-                <Button  variant='contained'>TRY NOW</Button>
+            </Typography>
+
+            <Grid item container xs={12} className={styles.btnGroup}>
+              <Grid item lg={2} sm={12}>
+                <Button variant='contained' className={styles.btn_try_now}>
+                  TRY NOW
+                </Button>
               </Grid>
-              <Grid item xs={10}>
-                <Button variant='contained'>FREE TILL 15JUN2023</Button>
+              <Grid item lg={10} sm={12}>
+                <Button variant='contained' className={styles.btn_free}>
+                  FREE TILL 15JUN2023
+                </Button>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={5} className={styles.mockups}>
-            <img src = {phones.src} className={styles.phones}/> 
+          <Grid item xl={5} md={5} xs={11} className={styles.mockups}>
+            <img src={phones.src} className={styles.phones} />
           </Grid>
         </Grid>
+
         <Grid item className={styles.content2} xs={12}>
           <div className={styles.bgImage}>
             <Grid item xs={12} className={styles.features}>
               <Grid item xs={3} className={styles.personalized}>
                 <div className={styles.persTittle}>
                   <div className={styles.persIcon} />
+                  <img src={gallary.src} className={styles.gallary_img} />
                   <h3>Personalized</h3>
                 </div>
                 <span>
@@ -44,14 +58,38 @@ export default function Home() {
                   preferences and location.
                 </span>
               </Grid>
-              <Grid item xs={4} className={styles.gallary}>
-                gallary
+              <Grid item xs={3} className={styles.gallary}>
+                <div className={styles.persTittle}>
+                  <div className={styles.persIcon} />
+                  <img src={gallary.src} className={styles.gallary_img} />
+
+                  <h3>Gallary</h3>
+                </div>
+                <span>
+                  A personalized weather report that shows the best time and
+                  place for couples to enjoy outdoor activities based on their
+                  preferences and location.
+                </span>
               </Grid>
-              <Grid item xs={4} className={styles.MoodTracker}>
-                MoodTracker
+              <Grid item xs={3} className={styles.MoodTracker}>
+                <div className={styles.persTittle}>
+                  <div className={styles.persIcon} />
+                  <img src={gallary.src} className={styles.gallary_img} />
+
+                  <h3>MoodTracker</h3>
+                </div>
+                <span>
+                  A personalized weather report that shows the best time and
+                  place for couples to enjoy outdoor activities based on their
+                  preferences and location.
+                </span>
               </Grid>
             </Grid>
           </div>
+        </Grid>
+
+        <Grid item className={styles.content3} xs={12}>
+          <div className={styles.nextsection}>Hello</div>
         </Grid>
       </Grid>
     </Box>
